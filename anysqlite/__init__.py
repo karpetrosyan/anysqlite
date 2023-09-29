3c23b3af466bfff316440577b39376990cab087f
karosis88 <kar.petrosyanpy@gmail.com>
2023-09-29 17:07:55 +0300

    Expose public API

--- anysqlite/__init__.py
+++ anysqlite/__init__.py
@@ -1,3 +1,22 @@
-from ._core import *  # noqa: F403
+from sqlite3 import (  # noqa: F401
+    DatabaseError,
+    DataError,
+    Error,
+    IntegrityError,
+    InterfaceError,
+    InternalError,
+    NotSupportedError,
+    OperationalError,
+    ProgrammingError,
+    Row,
+    Warning,
+    apilevel,
+    paramstyle,
+    sqlite_version,
+    sqlite_version_info,
+    threadsafety,
+)
+
+from ._core import Connection, Cursor, connect  # noqa: F401
 
 __version__ = "0.0.3"
