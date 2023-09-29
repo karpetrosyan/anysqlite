0ee1faca52fdfb620f6d2de586080f78643e1ba2
karosis88 <kar.petrosyanpy@gmail.com>
2023-09-29 16:57:22 +0300

    Use anyio.to_thread instead of polling

--- anysqlite/_core.py
+++ anysqlite/_core.py
@@ -1,28 +1,15 @@
-import asyncio
 import sqlite3
 import typing as tp
-from functools import partial
-from queue import Queue
-from threading import Event, Thread
+from functools import partial, wraps
 
-import sniffio
-
-try:
-    import trio
-except ImportError:  # pragma: no cover
-    trio = None  # type: ignore
-
-__all__ = ["connect", "Connection", "Cursor"]
+from anyio import CapacityLimiter, to_thread
 
 P = tp.ParamSpec("P")
 T = tp.TypeVar("T")
 
-apilevel = "2.0"
-threadsafety = sqlite3.threadsafety
-paramstyle = sqlite3.paramstyle
-
 
 def copy_signature(fnc: tp.Callable[P, T]):
+    @wraps(fnc)
     def dec(fnc) -> tp.Callable[P, T]:
         def inner(*args, **kwargs) -> T:
             return fnc(*args, **kwargs)
@@ -32,167 +19,119 @@ def copy_signature(fnc: tp.Callable[P, T]):
     return dec
 
 
-class Future:
-    def __init__(self, task: tp.Callable[..., tp.Any]) -> None:
-        self.fnc = task
+def return_connection(
+    fnc: tp.Callable[P, T]
+) -> tp.Callable[P, tp.Coroutine[None, None, "Connection"]]:
+    @wraps(fnc)
+    def inner(*args, **kwargs):
+        return fnc(*args, **kwargs)
 
-    def result(self):
-        if hasattr(self, "_result"):
-            return self._result
-
-        if hasattr(self, "_exc"):
-            raise self._exc
-
-        raise RuntimeError("Invalid state")
-
-    def set_result(self, value: tp.Any) -> None:
-        self._result = value
-
-    def set_exception(self, exc: BaseException) -> None:
-        self._exc = exc
-
-    def done(self) -> bool:
-        return hasattr(self, "_result") or hasattr(self, "_exc")
-
-    async def _await(self):
-        backend = sniffio.current_async_library()
-        while True:
-            if self.done():
-                return self.result()
-
-            if backend == "trio":
-                await trio.sleep(0.0000001)
-            elif backend == "asyncio":
-                await asyncio.sleep(0.0000001)
-
-    def __await__(self):
-        return self._await().__await__()
+    return inner
 
 
 class Connection:
-    def __init__(self, database: str) -> None:
-        self._database = database
-        self._real_connection: tp.Union[sqlite3.Connection, None] = None
-        self._running = True
-        self._queue: Queue[Future] = Queue()
-        connected = Event()
-        Thread(target=self.run, args=(connected,), daemon=True).start()
-        connected.wait()
-
-    def run(self, connected: Event):
-        self._real_connection = sqlite3.connect(self._database)
-        connected.set()
-
-        while self._running:
-            future = self._queue.get()
-
-            try:
-                result = future.fnc()
-                future.set_result(result)
-            except BaseException as exc:
-                future.set_exception(exc)
-
-    async def _execute(self, fnc: tp.Callable[..., tp.Any]):
-        fut = Future(fnc)
-        self._queue.put(fut)
-        return await fut
+    def __init__(self, _real_connection: sqlite3.Connection) -> None:
+        self._real_connection = _real_connection
+        self._limiter = CapacityLimiter(1)
 
     @copy_signature(sqlite3.Connection.close)
     async def close(self, *args, **kwargs):
-        return await self._execute(
-            partial(self._real_connection.close, *args, **kwargs)
+        return await to_thread.run_sync(
+            self._real_connection.close, *args, **kwargs, limiter=self._limiter
         )
 
     @copy_signature(sqlite3.Connection.commit)
     async def commit(self, *args, **kwargs):
-        return await self._execute(
-            partial(self._real_connection.commit, *args, **kwargs)
+        return await to_thread.run_sync(
+            self._real_connection.commit, *args, **kwargs, limiter=self._limiter
         )
 
     @copy_signature(sqlite3.Connection.rollback)
     async def rollback(self, *args, **kwargs):
-        return await self._execute(
-            partial(self._real_connection.rollback, *args, **kwargs)
+        return await to_thread.run_sync(
+            self._real_connection.rollback, *args, **kwargs, limiter=self._limiter
         )
 
     async def cursor(self, *args, **kwargs) -> "Cursor":
-        return Cursor(
-            conn=self,
-            real_cursor=await self._execute(
-                partial(self._real_connection.cursor, *args, **kwargs)
-            ),
+        real_cursor = await to_thread.run_sync(
+            self._real_connection.cursor, *args, **kwargs, limiter=self._limiter
         )
+        return Cursor(real_cursor, self._limiter)
 
 
 class Cursor:
-    def __init__(self, conn: Connection, real_cursor: sqlite3.Cursor) -> None:
-        self._connection = conn
+    def __init__(self, real_cursor: sqlite3.Cursor, limiter: CapacityLimiter) -> None:
         self._real_cursor = real_cursor
+        self._limiter = limiter
 
     @property
-    @copy_signature(sqlite3.Cursor.description)
-    def description(self):
+    def description(self) -> str:
         return self._real_cursor.description
 
     @property
-    @copy_signature(sqlite3.Cursor.rowcount)
-    def rowcount(self):
+    def rowcount(self) -> int:
         return self._real_cursor.rowcount
 
+    @property
+    def arraysize(self) -> int:
+        return self._real_cursor.arraysize
+
     @copy_signature(sqlite3.Cursor.close)
-    async def close(self, *args, **kwargs):
-        return await self._connection._execute(
-            partial(self._real_cursor.close, *args, **kwargs)
+    async def close(self, *args, **kwargs) -> None:
+        await to_thread.run_sync(
+            self._real_cursor.close, *args, **kwargs, limiter=self._limiter
         )
 
     @copy_signature(sqlite3.Cursor.execute)
-    async def execute(self, *args, **kwargs):
-        return await self._connection._execute(
-            partial(self._real_cursor.execute, *args, **kwargs)
+    async def execute(self, *args, **kwargs) -> "Cursor":
+        real_cursor = await to_thread.run_sync(
+            self._real_cursor.execute, *args, **kwargs, limiter=self._limiter
         )
+        return Cursor(real_cursor, self._limiter)
 
     @copy_signature(sqlite3.Cursor.executemany)
-    async def executemany(self, *args, **kwargs):
-        return await self._connection._execute(
-            partial(self._real_cursor.executemany, *args, **kwargs)
+    async def executemany(self, *args, **kwargs) -> "Cursor":
+        real_cursor = await to_thread.run_sync(
+            self._real_cursor.executemany, *args, **kwargs, limiter=self._limiter
         )
+        return Cursor(real_cursor, self._limiter)
+
+    @copy_signature(sqlite3.Cursor.executescript)
+    async def executescript(self, *args, **kwargs) -> "Cursor":
+        real_cursor = await to_thread.run_sync(
+            self._real_cursor.executescript, *args, **kwargs, limiter=self._limiter
+        )
+        return Cursor(real_cursor, self._limiter)
 
     @copy_signature(sqlite3.Cursor.fetchone)
     async def fetchone(self, *args, **kwargs):
-        return await self._connection._execute(
-            partial(self._real_cursor.fetchone, *args, **kwargs)
+        return await to_thread.run_sync(
+            self._real_cursor.fetchone, *args, **kwargs, limiter=self._limiter
         )
 
     @copy_signature(sqlite3.Cursor.fetchmany)
     async def fetchmany(self, *args, **kwargs):
-        return await self._connection._execute(
-            partial(self._real_cursor.fetchmany, *args, **kwargs)
+        return await to_thread.run_sync(
+            self._real_cursor.fetchmany, *args, **kwargs, limiter=self._limiter
         )
 
     @copy_signature(sqlite3.Cursor.fetchall)
     async def fetchall(self, *args, **kwargs):
-        return await self._connection._execute(
-            partial(self._real_cursor.fetchall, *args, **kwargs)
-        )
-
-    @copy_signature(sqlite3.Cursor.arraysize)
-    async def arraysize(self, *args, **kwargs):
-        return await self._connection._execute(
-            partial(self._real_cursor.arraysize, *args, **kwargs)
-        )
-
-    @copy_signature(sqlite3.Cursor.setinputsizes)
-    async def setinputsizes(self, *args, **kwargs):
-        return await self._connection._execute(
-            partial(self._real_cursor.setinputsizes, *args, **kwargs)
-        )
-
-    @copy_signature(sqlite3.Cursor.setoutputsize)
-    async def setoutputsize(self, *args, **kwargs):
-        return await self._connection._execute(
-            partial(self._real_cursor.setoutputsize, *args, **kwargs)
+        return await to_thread.run_sync(
+            self._real_cursor.fetchall, *args, **kwargs, limiter=self._limiter
         )
 
 
-def connect(database: str) -> Connection:
-    return Connection(database)
+@return_connection
+@copy_signature(sqlite3.connect)
+async def connect(*args, **kwargs) -> "Connection":
+    if len(args) >= 5:
+        args[4] = False
+
+    if "check_same_thread" in kwargs:
+        kwargs["check_same_thread"] = False
+
+    real_connection = await to_thread.run_sync(
+        partial(sqlite3.connect, *args, **kwargs, check_same_thread=False)
+    )
+    return Connection(real_connection)
