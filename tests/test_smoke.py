01aeee0911e7eb7ab7e812b9ac90843ac89f1355
karosis88 <kar.petrosyanpy@gmail.com>
2023-09-30 11:36:04 +0300

    Add requirements.txt

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,16 +1,20 @@
 import pytest
+
 import anysqlite
+
 
 @pytest.mark.anyio
 async def test_anysqlite():
     db = await anysqlite.connect(":memory:")
-    
+
     try:
         cursor = await db.cursor()
         await cursor.execute("CREATE TABLE anysqlite_test(version TEXT)")
-        await cursor.execute("INSERT INTO anysqlite_test VALUES(?)", [anysqlite.__version__])
+        await cursor.execute(
+            "INSERT INTO anysqlite_test VALUES(?)", [anysqlite.__version__]
+        )
         await cursor.execute("SELECT * FROM anysqlite_test")
 
-        assert (anysqlite.__version__, ) == await cursor.fetchone()
+        assert (anysqlite.__version__,) == await cursor.fetchone()
     finally:
         await db.close()
