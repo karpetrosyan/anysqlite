f5c45880b700ab44c377cd89d15b066296231a28
Waket Zheng <waketzheng@gmail.com>
2026-06-13 15:52:27 +0800

    Fix mypy complaints

--- anysqlite/_core.py
+++ anysqlite/_core.py
@@ -40,7 +40,10 @@ class Connection:
 
     async def execute(self, sql: str, parameters: tp.Iterable[tp.Any] = ()) -> "Cursor":
         real_cursor = await to_thread.run_sync(
-            self._real_connection.execute, sql, parameters, limiter=self._limiter
+            self._real_connection.execute,  # type: ignore[arg-type]
+            sql,
+            parameters,
+            limiter=self._limiter,
         )
         return Cursor(real_cursor, self._limiter)
 
@@ -48,7 +51,7 @@ class Connection:
         self, sql: str, seq_of_parameters: tp.Iterable[tp.Iterable[tp.Any]]
     ) -> "Cursor":
         real_cursor = await to_thread.run_sync(
-            self._real_connection.executemany,
+            self._real_connection.executemany,  # type: ignore[arg-type]
             sql,
             seq_of_parameters,
             limiter=self._limiter,
@@ -86,7 +89,10 @@ class Cursor:
 
     async def execute(self, sql: str, parameters: tp.Iterable[tp.Any] = ()) -> "Cursor":
         real_cursor = await to_thread.run_sync(
-            self._real_cursor.execute, sql, parameters, limiter=self._limiter
+            self._real_cursor.execute,  # type: ignore[arg-type]
+            sql,
+            parameters,
+            limiter=self._limiter,
         )
         return Cursor(real_cursor, self._limiter)
 
@@ -94,7 +100,10 @@ class Cursor:
         self, sql: str, seq_of_parameters: tp.Iterable[tp.Iterable[tp.Any]]
     ) -> "Cursor":
         real_cursor = await to_thread.run_sync(
-            self._real_cursor.executemany, sql, seq_of_parameters, limiter=self._limiter
+            self._real_cursor.executemany,  # type: ignore[arg-type]
+            sql,
+            seq_of_parameters,
+            limiter=self._limiter,
         )
         return Cursor(real_cursor, self._limiter)
 
@@ -120,9 +129,7 @@ class Cursor:
         )
 
 
-async def connect(
-    database: str | bytes | Path, **kwargs: tp.Any
-) -> "Connection":
+async def connect(database: str | bytes | Path, **kwargs: tp.Any) -> "Connection":
     kwargs["check_same_thread"] = False
     real_connection = await to_thread.run_sync(
         partial(sqlite3.connect, database, **kwargs)
